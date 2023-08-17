cf386c642dcad13feb2f07135b08d302f2dd5e56
ShenuhX <141113311+ShenuhX@users.noreply.github.com>
2023-08-17 12:18:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94740804536', 'Dinuwa Official²⁰²³', true],
-  ['94740804536', 'Dinuwa Official²⁰²³', true], 
+  ['94762000126', 'Noziy Official²⁰²³', true],
+  ['94762000126', 'Noziy Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
