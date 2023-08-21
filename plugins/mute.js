cbfb71f218559bea913f7c91a9f5059c0b96164c
ShenuhX <141113311+ShenuhX@users.noreply.github.com>
2023-08-21 11:19:39 +0530

    👸 Princess-Noziy 👸

--- plugins/mute.js
+++ plugins/mute.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('💝 Queen Hentai WA Bot 💝 mute this group')
+    m.reply('👸 Princess-Noziy 👸 WA Bot  mute this group')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
