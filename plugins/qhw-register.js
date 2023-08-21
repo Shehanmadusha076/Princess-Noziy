e2f441e2073ecaaa29b3875659b8dc0647db154b
ShenuhX <141113311+ShenuhX@users.noreply.github.com>
2023-08-21 11:07:58 +0530

    👸 Princess-Noziy 👸

--- plugins/qhw-register.js
+++ plugins/qhw-register.js
@@ -5,30 +5,30 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) throw `💝 You are already registered with 💝 Queen Hentai 💝\n\n🇱🇰 Do you want to re-register to 💝 Queen Hentai 💝 ?🇱🇰\n\n 🇱🇰 Use this command to remove your register 🇱🇰\n*.unhentai* (Serial number)`
-  if (!Reg.test(text)) throw `🇱🇰 Format incorrect\n\n 🇱🇰 Use this command of 💝 Queen Hentai 💝: *.qhw name.age*\n💝Exemple : *.qhw* Dinuwa.16`
+  if (user.registered === true) throw `👸 You are already registered with 👸 Princess-Noziy 👸\n\n Do you want to re-register to 👸 Princess-Noziy 👸 ?\n\nUse this command to remove your register\n*.unreg* (Serial number)`
+  if (!Reg.test(text)) throw ` Format incorrect\n\n  Use this command of 👸 Princess-Noziy 👸: *.qhw name.age*\nExemple : *.qhw* Noziy.18`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw '💝 The name cannot be empty'
-  if (!age) throw '💝 age cannot be empty'
-  if (name.length >= 30) throw '💝 The name is too long' 
+  if (!name) throw 'The name cannot be empty'
+  if (!age) throw 'age cannot be empty'
+  if (name.length >= 30) throw 'The name is too long' 
   age = parseInt(age)
-  if (age > 100) throw '💝 හම්මට නාකියා wants to play with 💝 Queen Hentai 💝 bot'
-  if (age < 5) throw '💝  there is a Carima podi eka '
+  if (age > 100) throw 'Do you wants to play with 👸 Princess-Noziy 👸 bot'
+  if (age < 5) throw 'There is a lol child'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
-┌─*💝 Queen Hentai 💝*─
-💝 *Registered*
-💝 *NUMBER:* ${name}
-💝 *AGE* : ${age} years
-💝 *SERIEL NUMBER* :
+┌─*👸 Princess-Noziy 👸*─
+👸 *Registered*
+👸 *NUMBER:* ${name}
+👸 *AGE* : ${age} years
+👸 *SERIEL NUMBER* :
 ${sn}
 └──────────────
 
- 💝 Queen Hentai 💝
+👸 Princess-Noziy 👸
 `.trim())
 }
 handler.help = ['reg'].map(v => v + ' <name.age>')
