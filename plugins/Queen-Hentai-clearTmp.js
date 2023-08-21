48f8ccc6bce3ce65ea60bd090c2d657486d55f83
ShenuhX <141113311+ShenuhX@users.noreply.github.com>
2023-08-21 11:00:19 +0530

    👸 Princess-Noziy 👸

--- plugins/Queen-Hentai-clearTmp.js
+++ plugins/Queen-Hentai-clearTmp.js
@@ -9,7 +9,7 @@ import {
 
 let handler = async (m, { conn, __dirname, args }) => {
 
-m.reply(`🇱🇰 The folder was cleaned *tmp + sessions*`)
+m.reply(` The folder was cleaned *tmp + sessions*`)
 m.react(done)
 const tmp = [tmpdir(), join(__dirname, '../tmp')]
   const filename = []
